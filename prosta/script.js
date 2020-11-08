66efbd1d18ea1eb208f00c6df9411c5c198b8bde
giorgimamardashvili <eanjaparidze75@gmail.com>
2020-11-08 21:55:21 +0400

    axali

---
+++ prosta/script.js
@@ -0,0 +1,49 @@
+// pirveli davaleba
+
+// let admin;
+// let name = 'giorgi mamardashvili';
+
+// admin = name;
+
+// console.log(admin);
+
+
+// meore
+
+// let name = "David";
+// console.log( `hello ${1}` ); // hellos miewereba 1
+// console.log( `hello ${"name"}` ); // hellos miewereba sityva name
+// console.log( `hello ${name}` ); // miewereba sityva david, romelic mivanichet names
+
+
+// mesame
+
+// let num = 50;
+
+// while (true) {
+//     let x = +prompt('sheiyvane ricxvi:', '');
+//     if (x < num) {
+//         alert('arasakmarisi mniSvneloba');
+//     } else if (x > num) {
+//         alert('zedmetia');
+//     } else {
+//         alert('zustad!');
+//         break;
+//     }
+// }
+
+
+// meotxe
+
+let birthYear = +prompt('sheiyvanet dabadebis weli', '');
+let currentYear = 2020;
+let age = currentYear - birthYear;
+
+if (age >= 18) {
+    alert('tqven xart srulwlovani');
+} else {
+    alert('tqven xart arasrulwlovani');
+}
+
+
+
