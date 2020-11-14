ab7412823480f4b2bac9e32b6993c8ab46722cb6
giorgimamardashvili <eanjaparidze75@gmail.com>
2020-11-14 13:48:44 +0400

    axali

--- prosta/script.js
+++ prosta/script.js
@@ -35,15 +35,68 @@
 
 // meotxe
 
-let birthYear = +prompt('sheiyvanet dabadebis weli', '');
-let currentYear = 2020;
-let age = currentYear - birthYear;
+// let birthYear = +prompt('sheiyvanet dabadebis weli', '');
+// let currentYear = 2020;
+// let age = currentYear - birthYear;
 
-if (age >= 18) {
-    alert('tqven xart srulwlovani');
-} else {
-    alert('tqven xart arasrulwlovani');
+// if (age >= 18) {
+//     alert('tqven xart srulwlovani');
+// } else {
+//     alert('tqven xart arasrulwlovani');
+// }
+
+// 13.11 პირველი დავალება
+
+// alert(null || 2 && 3 || 4); //პრიორიტეტულია და მნიშვნელობა. ამ შემთხვევაში 2 და 3 შორის ვირჩევთ 3, რადგან ბოლო true 3-ია. null არის false, მისი შემდეგი true არის სწორი პასუხი, ანუ 3.
+
+// მეორე დავალება
+
+// let age = +prompt("tqveni asaki", "");
+// alert(age >= 14 && age <= 90 ? 'true' : 'false');
+
+// მესამე დავალება
+
+// let login = prompt('Enter your login', '');
+
+// if (login === 'Admin') {
+//     let password = prompt('enter your password', '');
+
+//     if (password === 'TheMaster') {
+//         alert('Welcome!');
+//     } else if (password === '' || password === null) {
+//         alert('Canceled');
+//     } else {
+//         alert('Wrong password');
+//     }
+// } else if (login === '' || login === null) {
+//     alert('Canceled');
+// } else {
+//     alert('i dont know you');
+// }
+
+// მეოთხე დავალება
+
+// function checkAge(age) {
+//     let conf = confirm('Did parents allow you?');
+//     if (age > 18 || conf === true) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// checkAge();
+
+// მეხუთე დავალება
+
+function min(a, b) {
+    switch (a, b) {
+        case a > b:
+            return b;
+            break;
+        case a < b:
+            return a;
+            break;
+    }
 }
-
-
 
